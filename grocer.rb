928aa98a8af2724fd6694b7ed127dad9c5359c6a
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 19:35:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,48 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |x|
+    if new_cart[x.keys[0]]
+      new_cart[x.keys[0]][:count] += 1
+    else
+      new_cart[x.keys[0]] = {
+        count: 1,
+        price: x.values[0][:price],
+        clearance: x.values[0][:clearance]
+      }
+    end
+  end
+  new_cart
+  #binding.pry
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |x|
+    if cart.keys.include? x[:item]
+      if cart[x[:item]][:count] >= x[:num]
+        is_coupon = "#{x[:item]} W/COUPON"
+        if cart[is_coupon]
+          cart[is_coupon][:count] += x[:num]
+        else
+          cart[is_coupon] = {
+            count: x[:num],
+            price: x[:cost]/x[:num],
+            clearance: cart[x[:item]][:clearance]
+          }
+        end
+        cart[x[:item]][:count] -= x[:num]
+      end
+    end
+  end
+  cart
+  #binding.pry
 end
 
 def apply_clearance(cart)
-  # code here
+#binding.pry
 end
 
 def checkout(cart, coupons)
-  # code here
+#binding.pry
 end
